f36dda93c95837bfe34fb701405ee04926f8bb7d
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-06 23:10:32 +0100

    Made delivery options interactive

--- 13-amazon-project/scripts/checkout.js
+++ 13-amazon-project/scripts/checkout.js
@@ -2,7 +2,8 @@ import {
   cart, 
   removeFromCart, 
   calculateCartQuantity,
-  updateQuantity
+  updateQuantity,
+  updateDeliveryOption
 } from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
@@ -118,7 +119,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
 
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
       <input type="radio"
         ${isChecked ? 'checked' : ''}
         class="delivery-option-input"
@@ -150,6 +153,14 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
 
       updateCartQuantity();
+    });
+  });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
     });
   });
 
