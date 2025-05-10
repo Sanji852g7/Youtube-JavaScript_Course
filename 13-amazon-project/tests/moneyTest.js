6ca3218b6451d693fc246bd47ff7f3474a531d94
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-10 14:52:42 +0100

    Removed default tests and added money tests

--- 13-amazon-project/tests/moneyTest.js
+++ 13-amazon-project/tests/moneyTest.js
@@ -20,7 +20,7 @@ if (formatCurrency(0) === '0.00') {
 
 console.log('rounds up to nearest cent');
 
-if (formatCurrency(2000.5 === '20.02')) {
+if (formatCurrency(2000.5 === '20.01')) {
   console.log('passed');
 } else {
   console.log('failed');
