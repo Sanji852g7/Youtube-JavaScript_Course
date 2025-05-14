f66e33bb58ef93244d5f11a90a74b0e3da031344
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-14 21:34:46 +0100

    Created automated tests for OrderSummary

--- 13-amazon-project/scripts/checkout/orderSummary.js
+++ 13-amazon-project/scripts/checkout/orderSummary.js
@@ -29,7 +29,8 @@ export function renderOrderSummary(){
     const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
-    <div class="cart-item-container 
+    <div class="cart-item-container
+      js-cart-item-container 
       js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: ${dateString}
@@ -46,7 +47,8 @@ export function renderOrderSummary(){
           <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity
+          js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -59,7 +61,7 @@ export function renderOrderSummary(){
                 data-product-id="${matchingProduct.id}">
                 Save
               </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -135,14 +137,14 @@ export function renderOrderSummary(){
       });
     });
 
-  function updateCartQuantity() {
+  /* function updateCartQuantity() {
     const cartQuantity = calculateCartQuantity();
 
     document.querySelector('.js-return-to-home-link')
       .innerHTML = `${cartQuantity} items`;
   }
 
-  updateCartQuantity();
+  updateCartQuantity(); */
 
   document.querySelectorAll('.js-update-link')
     .forEach((link) => {
