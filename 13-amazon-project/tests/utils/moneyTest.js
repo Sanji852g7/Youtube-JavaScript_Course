6e0e521337aad1995e43069685c3cff41b3fba04
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-16 17:00:19 +0100

    Finished testing framework exercises

--- 13-amazon-project/tests/utils/moneyTest.js
+++ 13-amazon-project/tests/utils/moneyTest.js
@@ -12,4 +12,12 @@ describe('test suite: formatCurrency', () => {
   it('rounds up to nearest cents', () => {
     expect(formatCurrency(2000.5)).toEqual('20.01');
   });
+
+  it('rounds down to the nearest cents', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
+
+  it('works with negative numbers', () => {
+    expect(formatCurrency(-100)).toEqual('-1.00');
+  });
 });
