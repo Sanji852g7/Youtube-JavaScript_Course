91fe760bc386a1e3499f79aa88f973f8395dcee6
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-19 15:56:18 +0100

    Created class version of cart

--- 13-amazon-project/scripts/checkout.js
+++ 13-amazon-project/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary} from './checkout/paymentSummary.js';
-import '../data/cart-oop.js'; // This syntax runs all the code inside this file without importing anything
+import '../data/cart-class.js'; // This syntax runs all the code inside this file without importing anything
 
 renderCheckoutHeader();
 renderOrderSummary();
