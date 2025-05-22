808ce105e820bfdbe682ec838dc8f94a6f277d1f
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-22 18:37:54 +0100

    Finished challenge exercises

--- 13-amazon-project/scripts/amazon.js
+++ 13-amazon-project/scripts/amazon.js
@@ -1,90 +1,79 @@
-import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
+import {cart} from '../data/cart-class.js';
 import {products} from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
-
 products.forEach((product) => {
   productsHTML += `
-<div class="product-container">
-  <div class="product-image-container">
-    <img class="product-image"
-      src="${product.image}">
-  </div>
-
-  <div class="product-name limit-text-to-2-lines">
-    ${product.name}
-  </div>
-
-  <div class="product-rating-container">
-    <img class="product-rating-stars"
-      src="${product.getStarsUrl()}">
-    <div class="product-rating-count link-primary">
-      ${product.rating.count}
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="${product.getStarsUrl()}">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+      <div class="product-price">
+        ${product.getPrice()}
+      </div>
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      ${product.extraInfoHTML()}
+      <div class="product-spacer"></div>
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
     </div>
-  </div>
-
-  <div class="product-price">
-    ${product.getPrice()}
-  </div>
-
-  <div class="product-quantity-container">
-    <select class="js-quantity-selector-${product.id}">
-      <option selected value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-    </select>
-  </div>
-
-  ${product.extraInfoHTML( )}
-
-  <div class="product-spacer"></div>
-
-  <div class="added-to-cart js-added-to-cart-${product.id}">
-    <img src="images/icons/checkmark.png">
-    Added
-  </div>
-
-  <button class="add-to-cart-button button-primary js-add-to-cart"
-  data-product-id="${product.id}">
-    Add to Cart
-  </button>
-</div>
   `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
 function updateCartQuantity() {
-  const cartQuantity = calculateCartQuantity();
+  let cartQuantity = 0;
+
+  cart.cartItems.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
 }
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      cart.addToCart(productId);
+      updateCartQuantity();
+    });
+  });
 
-updateCartQuantity();
-
-
-// We're going to use an object to save the timeout ids.
-// The reason we use an object is because each product
-// will have its own timeoutId. So an object lets us
-// save multiple timeout ids for different products.
-// For example:
-// {
-//   'product-id1': 2,
-//   'product-id2': 5,
-//   ...
-// }
-// (2 and 5 are ids that are returned when we call setTimeout).
-const addedMessageTimeouts = {};
-function addMessage(productId){
+/* exercise
+  const addedMessageTimeouts = {};
+  function addMessage(productId){
   const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
 
       addedMessage.classList.add('added-to-cart-visible');
@@ -101,13 +90,8 @@ function addMessage(productId){
       // so we can stop it later if we need to.
       addedMessageTimeouts[productId] = timeoutId;
 }
+*/
 
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const {productId} = button.dataset;
-      addToCart(productId);
-      updateCartQuantity();
-      addMessage(productId);
-    });
-  });
+
+
+
