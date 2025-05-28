ef7794b0c7a3c980ea1484ab8112fee588b7bdb9
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-28 23:59:36 +0100

    Learned promises and convert code to promises

--- 13-amazon-project/scripts/checkout.js
+++ 13-amazon-project/scripts/checkout.js
@@ -2,13 +2,56 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js'; // This syntax runs all the code inside this file without importing anything
 // import '../data/car.js';
 // import '../data/backend-practice.js';
 
-  loadProducts(()=> {
-    renderCheckoutHeader();
-    renderOrderSummary();
-    renderPaymentSummary();
+
+Promise.all([
+  new Promise ((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(value);
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
+new Promise ((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
   });
-  
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+  loadProducts(()=> {
+    loadCart(() => {
+      renderCheckoutHeader();
+      renderOrderSummary();
+      renderPaymentSummary();
+    });
+  });
+  */
