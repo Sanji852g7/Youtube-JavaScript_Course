d045061836a5e7350de1338bc325fcdf95d0fcf3
Sanji Gurung <sanji.gurung@hotmail.com>
2025-05-29 14:56:08 +0100

    Converted loadProducts from XHR to fetch

--- 13-amazon-project/data/products.js
+++ 13-amazon-project/data/products.js
@@ -110,6 +110,31 @@ const object3 = {
 
 export let products = [];
 
+//Using fetch() -> it is better way to make http request bc it uses promises directly
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+  });
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', () => {
